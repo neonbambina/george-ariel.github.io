e795b6ead64f4c1c128b10fd0eac2f222d85cce8
Ariel <geo15006@byui.edu>
2018-06-08 21:33:04 -0600

    added grid!

--- cit230/assignments/lesson-7/scripts/windchill.js
+++ cit230/assignments/lesson-7/scripts/windchill.js
@@ -1,14 +1,10 @@
-var t = 18;
-var s = 20;
+var t = 90;
+var s = 5;
 var f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
 
+
+document.getElementById("temp").innerHTML = t;
+document.getElementById("windSpeed").innerHTML = s;
 document.getElementById("wcyo").innerHTML = f.toFixed(2);
-document.getElementById("wcyo").innerHTML = t;
-document.getElementById("wcyo").innerHTML = s;
 
     
-
- 
-
-//current conditions
-//span for temp, (id = temp)
